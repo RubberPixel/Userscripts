1d23a50b42d8846fd4667e70400c768609d3f139
RubberPixel <111243464+RubberPixel@users.noreply.github.com>
2025-06-08 16:12:19 +0000

    Create xhamster_watched_remover.js

---
+++ xhamster_watched_remover.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         XHamster "Watched" videos remover
+// @namespace    http://tampermonkey.net/
+// @version      2024-11-25
+// @license      MIT
+// @description  Removes thumbnails in XHamster for already watched videos
+// @author       RubberPixel
+// @match        https://xhamster.com/videos/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=xhamster.com
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// ==/UserScript==
+ 
+function GM_addStyle(cssStr){
+ 
+    var n = document.createElement('style');
+    n.type = "text/css";
+    n.innerHTML = cssStr;
+    document.getElementsByTagName('body')[0].appendChild(n);
+ 
+}
+ 
+GM_addStyle(".thumb-list__item:has(> a > div > div.thumb-image-container__watched) { visibility: collapse; }");
